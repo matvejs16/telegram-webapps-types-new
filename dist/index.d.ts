75af8636aef3a1ebbf42b7780a26c83b4359001d
matvejs16 <74150728+matvejs16@users.noreply.github.com>
2024-04-09 17:53:13 +0100

    feat: Update to Telegram Mini Apps v7.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -466,6 +466,73 @@ export declare namespace TelegramWebApps {
         getKeys(callback: (error: Error | null, keys?: any) => void): CloudStorage;
     }
 
+    interface BiometricManager {
+        /**
+         * Bot API 7.2+ Shows whether biometrics object is initialized.
+         */
+        isInited: boolean;
+        /**
+         * Bot API 7.2+ Shows whether biometrics is available on the current device.
+         */
+        isBiometricAvailable: boolean;
+        /**
+         * Bot API 7.2+ The type of biometrics currently available on the device.
+         * 
+         * Can be one of these values:
+         * - finger, fingerprint-based biometrics,
+         * - face, face-based biometrics,
+         * - unknown, biometrics of an unknown type.
+         */
+        biometricType: string;
+        /**
+         * Bot API 7.2+ Shows whether permission to use biometrics has been requested.
+         */
+        isAccessRequested: boolean;
+        /**
+         * Bot API 7.2+ Shows whether permission to use biometrics has been granted.
+         */
+        isAccessGranted: boolean;
+        /**
+         * Bot API 7.2+ Shows whether the token is saved in secure storage on the device.
+         */
+        isBiometricTokenSaved: boolean;
+        /**
+         * Bot API 7.2+ A unique device identifier that can be used to match the token to the device.
+         */
+        deviceId: string;
+        /**
+         * Bot API 7.2+ A method that initializes the BiometricManager object.
+         * 
+         * It should be called before the object's first use.
+         * If an optional callback parameter was passed, the callback function will be called when the object is initialized.
+         */
+        init(callback?: () => void): BiometricManager;
+        /**
+         * Bot API 7.2+ A method that requests permission to use biometrics according to the params argument of type [BiometricRequestAccessParams](https://core.telegram.org/bots/webapps#biometricrequestaccessparams).
+         * 
+         * If an optional callback parameter was passed, the callback function will be called and the first argument will be a boolean indicating whether the user granted access.
+         */
+        requestAccess(params: { reason?: string }, callback?: (granted: boolean) => void): BiometricManager;
+        /**
+         * Bot API 7.2+ A method that authenticates the user using biometrics according to the params argument of type [BiometricAuthenticateParams](https://core.telegram.org/bots/webapps#biometricauthenticateparams).
+         * 
+         * If an optional callback parameter was passed, the callback function will be called and the first argument will be a boolean indicating whether the user authenticated successfully. If so, the second argument will be a biometric token.
+         */
+        authenticate(params: { reason?: string }, callback?: (success: boolean, token?: string) => void): BiometricManager;
+        /**
+         * Bot API 7.2+ A method that updates the biometric token in secure storage on the device.
+         * 
+         * To remove the token, pass an empty string. If an optional callback parameter was passed, the callback function will be called and the first argument will be a boolean indicating whether the token was updated.
+         */
+        updateBiometricToken(token: string, callback?: (updated: boolean) => void): BiometricManager;
+        /**
+         * Bot API 7.2+ A method that opens the biometric access settings for bots. Useful when you need to request biometrics access to users who haven't granted it yet.
+         * 
+         * *Note that this method can be called only in response to user interaction with the Mini App interface (e.g. a click inside the Mini App or on the main button)*
+         */
+        openSettings(): BiometricManager;
+    }
+
     interface SettingsButton {
         /**
          * 	Shows whether the context menu item is visible. Set to false by default.
